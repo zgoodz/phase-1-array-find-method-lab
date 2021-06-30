4784c58c6cb38c7984a41747c1cfefcd42449d2c
Zachary Goodman <znote89@gmail.com>
2021-06-30 09:50:46 -0400

    commit tree c57bdfabdddcf5608e618c03c7a9b598af4b5ef1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(array) {
+    let winningYear = array.find(array => array.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    }
+  }
+  superbowlWin(record);
